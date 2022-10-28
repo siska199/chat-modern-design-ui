db61a0e34a4ba8a85d1dd6bfd9ca670a1c719829
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-10-28 11:00:56 +0700

    fix card contacts ovefloe for long messages

--- src/atoms/Contact.tsx
+++ src/atoms/Contact.tsx
@@ -9,13 +9,13 @@ const Contact: React.FC<TContactData & {handleContactActive:(id:string)=>void}> 
   const {state:{activeContactData}} = useContext(ChatContext)
   const {username, image, lastMessage, notif,status, handleContactActive, id} = props
   return (
-    <div onClick={()=>handleContactActive(id)} className={`${activeContactData?.id===id&&"bg-cd700"} p-3 flex justify-between hover:bg-cd700 cursor-pointer`}>
+    <div onClick={()=>handleContactActive(id)} className={`${activeContactData?.id===id&&"bg-cd700"} p-3 flex justify-between hover:bg-cd700 cursor-pointer max-h-[6rem] gap-2`}>
       <SumContactInfo type="contact" username={username} status={status} image={image} info={lastMessage ? lastMessage?.text:"Click to send messsage"}/>
-      <div className='flex flex-col gap-2 justify-center'>
+      <div className='flex flex-col gap-2 justify-center '>
         {
           lastMessage&&(
             <>
-              <span className='text-[0.6rem] text-cd500'>{formatDateForSumContactInfo(lastMessage.createdAt)}</span>
+              <span className='text-[0.6rem] text-cd500 w-[5rem]'>{formatDateForSumContactInfo(lastMessage.createdAt)}</span>
               {
                 notif!==0&&(
                   <span className='text-center text-[0.7rem] bg-main w-4 h-4 m-auto rounded-full'>{notif}</span>
--- src/atoms/SumContactInfo.tsx
+++ src/atoms/SumContactInfo.tsx
@@ -4,11 +4,11 @@ import Avatar from "./Avatar"
 const SumContactInfo : React.FC<TSumContactInfoProps> = (props) => {
   const {image, username, info,status, type,handleOnClickImage} = props
   return (
-    <div className="flex items-center gap-3">
+    <div className={`${type==="contact"&&"overflow-hidden"} flex items-center gap-3`}>
       <Avatar  handleOnClickImage={handleOnClickImage&&handleOnClickImage} online={status==="online"?true:false} url={image} sizeType="medium"/>
-      <div>
+      <div className="overflow-hidden">
         <h1 className={`${type==="contact"?"":"text-lg "} font-thin tracking-wide`}>{username}</h1>
-        <p className="text-[0.8rem] text-cd500 font-thin">{info}</p>
+        <p className="text-[0.8rem] text-cd500 font-thin truncate text-ellipsis">{info}</p>
       </div>      
     </div>
   )
